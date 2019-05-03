10e7e45dc2c351ac40771a18972fb97d7ea1b226
Varindra Hart <varindra@teacherspayteachers.com>
2019-05-03 13:56:44 -0400

    Initial Commit

---
+++ RTC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RTC
+//
+//  Created by Varindra Hart on 5/3/19.
+//  Copyright © 2019 VH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
